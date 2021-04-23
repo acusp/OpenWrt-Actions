961f9b95b884a1c1c4005ecb8ac38bf87173058d
acusp <acusp.xu@gmail.com>
2021-04-23 18:39:49 +0000

    Update: add dependency

--- pkg-list.sh
+++ pkg-list.sh
@@ -13,9 +13,15 @@ change_dir() {
     sed -i "s/..\/..\/luci.mk/\$\(TOPDIR\)\/feeds\/luci\/luci.mk/g" $1
 }
 
+mv_dir() {
+    [ -e $1/README.md ] && rm $1/README.md
+    mv $1/* . && rm -rf $1 
+}
+
 # ---< Common dependency >---
 #
 svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping
 
 # ---< Science online >---
 #
@@ -23,16 +29,19 @@ svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
 #git clone https://github.com/frainzy1477/luci-app-clash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 # PassWall
-git clone https://github.com/garypang13/smartdns-le
+git clone https://github.com/garypang13/smartdns-le # for luci-app-bypass
 git clone https://github.com/garypang13/luci-app-bypass
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall
+mv_dir openwrt-passwall 
 # SSR-Plus
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus
+git clone https://github.com/fw876/helloworld
+mv_dir helloworld
 git clone https://github.com/jerrykuku/luci-app-vssr
 
 # ---< Anti advertising >---
 #
 git clone https://github.com/garypang13/luci-app-dnsfilter
+svn co https://github.com/immortalwrt/packages/trunk/net/adguardhome # for luci-app-adguardhome
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-adguardhome
 change_dir luci-app-adguardhome/Makefile
 git clone https://github.com/project-lede/luci-app-godproxy
@@ -44,6 +53,7 @@ git clone https://github.com/BCYDTZ/luci-app-UUGameAcc
 # ---< Network >---
 #
 git clone https://github.com/garypang13/luci-app-eqos
+svn co https://github.com/garypang13/openwrt-packages/trunk/gost # for luci-app-gost
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gost
 change_dir luci-app-gost/Makefile
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-smartdns
